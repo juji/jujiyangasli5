fbf1602086ecf3d912340e5e87c1899649062bec
juji <him@jujiyangasli.com>
2024-12-21 20:08:21 +0700

    sveltekit headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-/** @type {import('vite').Plugin} */
-const viteServerConfig = () => ({
-	name: 'add-headers',
-	// @ts-expect-error
-	configureServer: (server) => {
-		// @ts-expect-error
-		server.middlewares.use((_, res, next) => {
-			res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-			next();
-		});
-	}
-});
+// /** @type {import('vite').Plugin} */
+// const viteServerConfig = () => ({
+// 	name: 'add-headers',
+// 	// @ts-expect-error
+// 	configureServer: (server) => {
+// 		// @ts-expect-error
+// 		server.middlewares.use((_, res, next) => {
+// 			res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+// 			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+// 			next();
+// 		});
+// 	}
+// });
 
 export default defineConfig({
-	plugins: [sveltekit(), viteServerConfig()],
+	plugins: [
+		sveltekit(), 
+		// viteServerConfig()
+	],
 	server: {
 		hmr: {
 			overlay: false
