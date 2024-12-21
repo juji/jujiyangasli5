8f10139721ac4d3f0056758814681373517e559f
juji <him@jujiyangasli.com>
2024-12-21 19:07:42 +0700

    CORS problem on play image

--- src/lib/data/play/data.server.ts
+++ src/lib/data/play/data.server.ts
@@ -5,19 +5,19 @@ export const play:Play[] = [
   {
     title: 'Grainy Thing',
     id: 'grainy-thing',
-    image: 'https://jujiplay.com/grainy-600.webp',
+    image: '/images/play/grainy-600.webp',
     url: 'https://grainy-thing.jujiplay.com/'
   },
   {
     title: 'Boid',
     id: 'Boid',
-    image: 'https://jujiplay.com/boids-600.webp',
+    image: '/images/play/boids-600.webp',
     url: 'https://boids.jujiplay.com/'
   },
   {
     title: 'Clifford-Dejong',
     id: 'CliffordDejong',
-    image: 'https://jujiplay.com/clifford-dejong-600.webp',
+    image: '/images/play/clifford-dejong-600.webp',
     url: 'https://wallpaper.jujiplay.com/'
   },
   // {
