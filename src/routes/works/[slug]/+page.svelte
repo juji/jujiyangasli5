525c641befe3ae4c55aa5d22e5024fcd0405df1f
juji <him@jujiyangasli.com>
2024-12-26 18:19:22 +0700

    multipe image for twitter and fb

--- src/routes/works/[slug]/+page.svelte
+++ src/routes/works/[slug]/+page.svelte
@@ -20,14 +20,18 @@
   <meta property="og:type" content="website" />
   <meta property="og:title" content={`juji: ${data.work.title}`} />
   <meta property="og:description" content={`Checkout juji's work: ${data.work.title}.`} />
-  <meta property="og:image" content={`${page.url.origin}${data.work.images[0].url}`} />
+  {#each data.work.images as image}
+    <meta property="og:image" content={`${page.url.origin}${image.url}`} />
+  {/each}
 
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content={`${page.url.host}`} />
   <meta property="twitter:url" content={`${page.url.href}`} />
   <meta name="twitter:title" content={`juji: ${data.work.title}`} />
   <meta name="twitter:description" content={`Checkout juji's work: ${data.work.title}.`} />
-  <meta name="twitter:image" content={`${page.url.origin}${data.work.images[0].url}`} />
+  {#each data.work.images as image}
+    <meta name="twitter:image" content={`${page.url.origin}${image.url}`} />
+  {/each}
 </svelte:head>
 
 <WorkDetails data={data} />
