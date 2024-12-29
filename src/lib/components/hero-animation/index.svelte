ba2d5e036bf0d3bd9b1ace38cd7ff1df40cb115c
juji <him@jujiyangasli.com>
2024-12-29 19:12:41 +0700

    refactor

--- src/lib/components/hero-animation/index.svelte
+++ src/lib/components/hero-animation/index.svelte
@@ -73,7 +73,7 @@
       if(n <= goodInterval){
 
         if(module === 'grainy-thing') return;
-        showSign('fps is 👍🏿, using "blob"')
+        showSign('fps is 💛, using "blob"')
         module = 'grainy-thing'
 
       }else if(module !== 'circular'){
